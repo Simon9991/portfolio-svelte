f5708e1f1ed4713cb3a4e40afc0c7d89407e1974
Simon Bernabeu <63356214+Simon9991@users.noreply.github.com>
2024-02-01 22:59:14 +0900

    font & other stuff

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import '../app.css';
+</script>
+
+<p>hello</p>
+<h1>TEST</h1>
+<slot />
+
+<style lang="scss">
+    p {
+        font-weight: 400;
+    }
+
+    h1 {
+        font-weight: 700;
+    }
+</style>
