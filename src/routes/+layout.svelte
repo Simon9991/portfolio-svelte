09454b27a22a458751e255e540a8abcaf01d06ce
Simon Bernabeu <63356214+Simon9991@users.noreply.github.com>
2024-02-03 00:24:27 +0900

    trying to do background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,4 @@
     import '../app.css';
 </script>
 
-<p>hello</p>
-<h1>TEST</h1>
 <slot />
-
-<style lang="scss">
-    p {
-        font-weight: 400;
-    }
-
-    h1 {
-        font-weight: 700;
-    }
-</style>
