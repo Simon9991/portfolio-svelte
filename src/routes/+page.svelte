618f435265c5fb03aa5b1d7c16fe984423c1c651
Simon Bernabeu <63356214+Simon9991@users.noreply.github.com>
2024-02-11 22:56:46 +0900

    refactor(home): changed some dummy data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,83 +1,98 @@
-<div class="flex flex-col mt-10 self-center gap-y-5">
-    <div class="card card-hover self-center">
+<!-- Scroll testing -->
+<div class="flex flex-col mt-10 gap-y-5">
+    <div class="card-hover self-center w-[600px]">
+        <h1 class="font-semibold">I am Simon Bernabeu</h1>
+        <p>
+            a <span class="font-semibold">french</span> software engineer currently living in
+            <span class="font-semibold">South Korea</span>. I thrive in solving problems and finding
+            solutions.
+        </p>
+        <p class="font-semibold">
+            My favorites languages are
+            <span class="font-normal"
+                ><kbd>C</kbd>, <kbd>C++</kbd>, <kbd>Svelte</kbd> (JavaScript framework).</span
+            >
+        </p>
+    </div>
+    <div class="card-hover self-center">
         <h2 class="font-semibold">About me</h2>
         <p>My name is Simon and I'm a software engineer.</p>
         <p>안녕하세요</p>
     </div>
 
-    <div class="card card-hover self-center">
+    <div class="card-hover self-center">
         <h2 class="font-semibold">About me</h2>
         <p>My name is Simon and I'm a software engineer.</p>
         <p>안녕하세요</p>
     </div>
 
-    <div class="card card-hover self-center">
+    <div class="card-hover self-center">
         <h2 class="font-semibold">About me</h2>
         <p>My name is Simon and I'm a software engineer.</p>
         <p>안녕하세요</p>
     </div>
 
-    <div class="card card-hover self-center">
+    <div class="card-hover self-center">
         <h2 class="font-semibold">About me</h2>
         <p>My name is Simon and I'm a software engineer.</p>
         <p>안녕하세요</p>
     </div>
 
-    <div class="card card-hover self-center">
+    <div class="card-hover self-center">
         <h2 class="font-semibold">About me</h2>
         <p>My name is Simon and I'm a software engineer.</p>
         <p>안녕하세요</p>
     </div>
 
-    <div class="card card-hover self-center">
+    <div class="card-hover self-center">
         <h2 class="font-semibold">About me</h2>
         <p>My name is Simon and I'm a software engineer.</p>
         <p>안녕하세요</p>
     </div>
 
-    <div class="card card-hover self-center">
+    <div class="card-hover self-center">
         <h2 class="font-semibold">About me</h2>
         <p>My name is Simon and I'm a software engineer.</p>
         <p>안녕하세요</p>
     </div>
 
-    <div class="card card-hover self-center">
+    <div class="card-hover self-center">
         <h2 class="font-semibold">About me</h2>
         <p>My name is Simon and I'm a software engineer.</p>
         <p>안녕하세요</p>
     </div>
 
-    <div class="card card-hover self-center">
+    <div class="card-hover self-center">
         <h2 class="font-semibold">About me</h2>
         <p>My name is Simon and I'm a software engineer.</p>
         <p>안녕하세요</p>
     </div>
 
-    <div class="card card-hover self-center">
+    <div class="card-hover self-center">
         <h2 class="font-semibold">About me</h2>
         <p>My name is Simon and I'm a software engineer.</p>
         <p>안녕하세요</p>
     </div>
 
-    <div class="card card-hover self-center">
+    <div class="card-hover self-center">
         <h2 class="font-semibold">About me</h2>
         <p>My name is Simon and I'm a software engineer.</p>
         <p>안녕하세요</p>
     </div>
 
-    <div class="card card-hover self-center">
+    <div class="card-hover self-center">
         <h2 class="font-semibold">About me</h2>
         <p>My name is Simon and I'm a software engineer.</p>
         <p>안녕하세요</p>
     </div>
 
-    <div class="card card-hover self-center">
+    <div class="card-hover self-center">
         <h2 class="font-semibold">About me</h2>
         <p>My name is Simon and I'm a software engineer.</p>
         <p>안녕하세요</p>
     </div>
 
-    <div class="card card-hover self-center">
+    <div class="card-hover self-center">
         <h2 class="font-semibold">About me</h2>
         <p>My name is Simon and I'm a software engineer.</p>
         <p>안녕하세요</p>
