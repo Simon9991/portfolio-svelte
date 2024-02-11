869f9790d3a7666227c58261ea251b8c7726ec25
Simon Bernabeu <63356214+Simon9991@users.noreply.github.com>
2024-02-11 22:02:03 +0900

    feat(parallax-scroll): parallax scrolling working, can be improved

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,49 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
     import '../app.css';
+
+    // Reactive variable for tracking the scroll position
+    let scrollY = 0;
+
+    let bodyElement: HTMLElement | null = null;
+    let navElement: HTMLElement | null = null;
+
+    onMount(() => {
+        const updateScroll = () => {
+            scrollY = window.pageYOffset;
+        };
+
+        window.addEventListener('scroll', updateScroll);
+
+        bodyElement = document.getElementsByTagName('body')[0];
+        navElement = document.getElementById('nav');
+
+        // TODO: change between light and dark theme
+        const bgImage = `/img/bg-dark-${Math.floor(Math.random() * 5) + 1}.jpg`;
+
+        if (bodyElement) {
+            bodyElement.style.backgroundImage = `url(${bgImage})`;
+        }
+
+        // Cleanup the event listener when the component is destroyed
+        return () => {
+            window.removeEventListener('scroll', updateScroll);
+        };
+    });
+
+    // Calculating the background position
+    $: backgroundPositionY = scrollY * 0.5;
+
+    $: {
+        if (bodyElement && navElement) {
+            bodyElement.style.backgroundPositionY = `${backgroundPositionY}px`;
+            // moves the nav element at the opposite of the background
+            // navElement.style.top = `-${scrollY / 1.5}px`;
+        }
+    }
 </script>
 
-<div class="top-nav mb-4">
+<div id="nav" class="top-nav h-24 mb-4 bg-slate-800 bg-opacity-0 backdrop-blur-md">
     <a href="/">
         <img src="/img/my_profile_pic.jpg" alt="Simon profile" />
     </a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,81 @@
     <p>My name is Simon and I'm a software engineer.</p>
     <p>안녕하세요</p>
 </div>
+
+<div class="card card-hover self-center">
+    <h2 class="font-semibold">About me</h2>
+    <p>My name is Simon and I'm a software engineer.</p>
+    <p>안녕하세요</p>
+</div>
+
+<div class="card card-hover self-center">
+    <h2 class="font-semibold">About me</h2>
+    <p>My name is Simon and I'm a software engineer.</p>
+    <p>안녕하세요</p>
+</div>
+
+<div class="card card-hover self-center">
+    <h2 class="font-semibold">About me</h2>
+    <p>My name is Simon and I'm a software engineer.</p>
+    <p>안녕하세요</p>
+</div>
+
+<div class="card card-hover self-center">
+    <h2 class="font-semibold">About me</h2>
+    <p>My name is Simon and I'm a software engineer.</p>
+    <p>안녕하세요</p>
+</div>
+
+<div class="card card-hover self-center">
+    <h2 class="font-semibold">About me</h2>
+    <p>My name is Simon and I'm a software engineer.</p>
+    <p>안녕하세요</p>
+</div>
+
+<div class="card card-hover self-center">
+    <h2 class="font-semibold">About me</h2>
+    <p>My name is Simon and I'm a software engineer.</p>
+    <p>안녕하세요</p>
+</div>
+
+<div class="card card-hover self-center">
+    <h2 class="font-semibold">About me</h2>
+    <p>My name is Simon and I'm a software engineer.</p>
+    <p>안녕하세요</p>
+</div>
+
+<div class="card card-hover self-center">
+    <h2 class="font-semibold">About me</h2>
+    <p>My name is Simon and I'm a software engineer.</p>
+    <p>안녕하세요</p>
+</div>
+
+<div class="card card-hover self-center">
+    <h2 class="font-semibold">About me</h2>
+    <p>My name is Simon and I'm a software engineer.</p>
+    <p>안녕하세요</p>
+</div>
+
+<div class="card card-hover self-center">
+    <h2 class="font-semibold">About me</h2>
+    <p>My name is Simon and I'm a software engineer.</p>
+    <p>안녕하세요</p>
+</div>
+
+<div class="card card-hover self-center">
+    <h2 class="font-semibold">About me</h2>
+    <p>My name is Simon and I'm a software engineer.</p>
+    <p>안녕하세요</p>
+</div>
+
+<div class="card card-hover self-center">
+    <h2 class="font-semibold">About me</h2>
+    <p>My name is Simon and I'm a software engineer.</p>
+    <p>안녕하세요</p>
+</div>
+
+<div class="card card-hover self-center">
+    <h2 class="font-semibold">About me</h2>
+    <p>My name is Simon and I'm a software engineer.</p>
+    <p>안녕하세요</p>
+</div>
