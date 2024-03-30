b31e6a5903ba2d2a08649792aeceb6ad88de8af4
Simon9991 <simon.bernabeu@epitech.eu>
2024-03-30 18:59:41 +0900

    style(project/[id]): 'card' preparation

--- src/routes/projects/[id]/+page.svelte
+++ src/routes/projects/[id]/+page.svelte
@@ -4,7 +4,9 @@
     export let data: PageData;
 </script>
 
-<div class="min-w-0 max-w-[70%] backdrop-blur-md bg-slate-700 bg-opacity-10 w-full h-24">
+<div
+    class="min-w-0 max-w-[70%] min-h-[300px] max-h-[70%] overflow-auto m-auto p-4 rounded-lg backdrop-blur-md bg-[#101720] bg-opacity-80 w-full"
+>
     <h2>{data.title}</h2>
     <!-- eslint-disable-next-line svelte/no-at-html-tags -->
     <div>{@html data.content}</div>
