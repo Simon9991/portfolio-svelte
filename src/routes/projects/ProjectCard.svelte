d1d436fbe40b235431ff06243010dcac72e2f710
Simon9991 <simon.bernabeu@epitech.eu>
2024-06-09 01:07:53 +0900

    feat(pnpm): migrated from npm to pnpm

--- src/routes/projects/ProjectCard.svelte
+++ src/routes/projects/ProjectCard.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { onMount } from 'svelte';
-    import { quadInOut } from 'svelte/easing';
-    import { fade } from 'svelte/transition';
+    import { Photo } from 'svelte-heros-v2';
 
     export let label: string;
     export let description: string; // Can be HTML string
@@ -27,46 +26,23 @@
 </script>
 
 <a href="/projects/{project_id}">
-    <div class="card-hover w-[600px] h-[400px] flex flex-col gap-4">
+    <div class="card-hover w-[600px] h-fit max-h-fit flex flex-col gap-4">
         <h2 class="font-semibold">{label}</h2>
         <p>{description}</p>
         <div class="end-section">
             {#if loading}
                 <div
-                    transition:fade={{ duration: 200, easing: quadInOut }}
-                    class="end-section-img flex flex-col items-center h-full justify-center"
+                    class="animate-pulse end-section-img bg-gray-300 flex items-center justify-center h-[270px]"
                 >
-                    <svg
-                        class="animate-spin h-20 w-20 self-center"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                    >
-                        <circle
-                            class="opacity-25"
-                            cx="12"
-                            cy="12"
-                            r="10"
-                            stroke="currentColor"
-                            stroke-width="4"
-                        ></circle>
-                        <path
-                            class="opacity-75"
-                            fill="currentColor"
-                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                        ></path>
-                    </svg>
+                    <Photo size="30" color="#9ca3af" />
                 </div>
             {:else if !loading && !error}
-                <img
-                    transition:fade={{ delay: 230, duration: 200, easing: quadInOut }}
-                    class="end-section-img"
-                    src={img_placeholder}
-                    alt="project example"
-                />
+                <img class="end-section-img" src={img_placeholder} alt="project example" />
+                <div class="w-full text-right text-sm text-opacity-75">
+                    Click on the card to see more
+                </div>
             {/if}
         </div>
-        <div class="w-full text-right text-sm text-opacity-75">Click on the card to see more</div>
     </div>
 </a>
 
@@ -79,6 +55,6 @@
 
     .end-section-img {
         @apply w-[80%];
-        @apply rounded-md opacity-90;
+        @apply rounded-md shadow-md opacity-90;
     }
 </style>
