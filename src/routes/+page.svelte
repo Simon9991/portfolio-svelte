95b79006661b86cc9d4c45808237f98d258659fb
Simon9991 <simon.bernabeu@epitech.eu>
2024-06-23 00:23:11 +0900

    wip: welcome page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,21 @@
 <!-- Scroll testing -->
-<div class="flex flex-col mt-10 gap-y-5">
-    <div class="card-hover self-center max-w-[80%] w-fit flex flex-col gap-2">
-        <h1 class="font-semibold">Hey, I am Simon Bernabeu!</h1>
+<div class="flex flex-col mt-10 gap-y-5 max-w-[850px] w-[850px] mx-auto">
+    <div class="card-hover self-center max-w-[850px] w-[850px] flex flex-col">
+        <h1 class="font-semibold mb-2">Hey, I am Simon Bernabeu!</h1>
         <p>
             A software engineer currently living in
             <span class="font-semibold">South Korea</span>. I thrive in solving problems and finding
             solutions.
         </p>
         <p>I am passionate about anything related to code, NeoVim and helping others.</p>
+    </div>
+
+    <div class="card mt-4 flex flex-col">
+        <h2 class="font-semibold w-fit">Blog Posts</h2>
+        <hr class="h-px mb-8 mt-2 border-0 bg-neutral-200 opacity-65" />
+        <div class="px-2">
+            <h3 class="font-semibold">This is a blog test</h3>
+        </div>
     </div>
     <div class="card-hover self-center flex flex-col gap-2">
         <h2 class="font-semibold">About me</h2>
