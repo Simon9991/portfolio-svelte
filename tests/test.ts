83565f83e7bfc3e88161dfec15fe5ade868ebd35
Simon9991 <simon.bernabeu@epitech.eu>
2024-06-26 23:00:26 +0900

    wip: trying to make `playwright` test working
    For now, fails...

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,14 @@
-import { expect, test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
     await page.goto('/');
     await expect(page.getByRole('heading', { name: 'Hey, I am Simon Bernabeu!' })).toBeVisible();
+});
+
+test('index page has navbar', async ({ page }) => {
+    await page.goto('/');
+
+    await expect(page.getByRole('link', { name: 'Blog' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Experience' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Socials' })).toBeVisible();
 });
