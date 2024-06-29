2f63ea74d9b2265ec4e8bd85ac3b053c6b273614
Simon9991 <simon.bernabeu@epitech.eu>
2024-06-29 17:58:55 +0900

    fix: client SDK is safe to include

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -1,24 +1,16 @@
 // Import the functions you need from the SDKs you need
 import firebase from 'firebase/compat/app';
 
-const API_KEY = import.meta.env.API_KEY || 'empty';
-const AUTH_DOMAIN = import.meta.env.AUTH_DOMAIN || 'empty';
-const PROJECT_ID = import.meta.env.PROJECT_ID || 'empty';
-const STORAGE_BUCKET = import.meta.env.STORAGE_BUCKET || 'empty';
-const MESSAGING_SENDER_ID = import.meta.env.MESSAGING_SENDER_ID || 'empty';
-const APP_ID = import.meta.env.APP_ID || 'empty';
-const MEASUREMENT_ID = import.meta.env.MEASUREMENT_ID || 'empty';
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: API_KEY,
-    authDomain: AUTH_DOMAIN,
-    projectId: PROJECT_ID,
-    storageBucket: STORAGE_BUCKET,
-    messagingSenderId: MESSAGING_SENDER_ID,
-    appId: APP_ID,
-    measurementId: MEASUREMENT_ID
+    apiKey: 'AIzaSyAR-WQRhOJ7PNkZVOQVWf2i6Bhdf3mKCRQ',
+    authDomain: 'simon-portfolio-9991.firebaseapp.com',
+    projectId: 'simon-portfolio-9991',
+    storageBucket: 'simon-portfolio-9991.appspot.com',
+    messagingSenderId: '672073603194',
+    appId: '1:672073603194:web:952af2d32290c450d343c3',
+    measurementId: 'G-F2PS4QNQHM'
 };
 
 // Initialize Firebase
