5acaff879f4087ec79e50420f1126f465ed8539d
Simon9991 <simon.bernabeu@epitech.eu>
2024-06-30 16:03:48 +0900

    feat(login): template `login` page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,20 @@
+<!-- "Hidden" route, should be accessible only for people who want an account to comment on the blog posts later -->
+<!-- For now used for admins to add blog posts -->
+<script lang="ts">
+    import { auth } from '@/lib/firebase/firebase';
+    import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+
+    async function signInWithGoogle() {
+        const provider = new GoogleAuthProvider();
+        const user = await signInWithPopup(auth, provider);
+        console.log(user);
+    }
+</script>
+
+<div class="card px-6 py-4 max-w-lg flex flex-col w-fit mx-auto gap-4">
+    <h1 class="uppercase">Login</h1>
+    <button
+        class="px-4 py-2 rounded-lg bg-purple-700 hover:brightness-110 transition-all"
+        onclick={signInWithGoogle}>Sign in with Google</button
+    >
+</div>
